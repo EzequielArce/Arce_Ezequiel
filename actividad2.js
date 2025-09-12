81385962c652c2a31e1f696f284ee0bcc2d579e6
EzequielArce <ezequielarce281@gmail.com>
2025-09-12 01:08:32 -0300

    Entrega Actividad 2

--- actividad2.js
+++ actividad2.js
@@ -1,6 +1,6 @@
 async function obtenerUsuario(url,indice){
     try{
-        const respuesta = await fetch(`${url}/${indice}`)
+        const respuesta = await fetch(url + '/' + indice)
         const usuarios = await respuesta.json()
         return usuarios
     }catch(e){
@@ -10,7 +10,7 @@ async function obtenerUsuario(url,indice){
 
 async function obtenerPosts(url,usuarioId){
     try{
-        const respuesta = await fetch(`${url}${usuarioId}`)
+        const respuesta = await fetch(url + usuarioId)
         const publicaciones = await respuesta.json()
         return publicaciones
     }catch(e){
@@ -24,31 +24,30 @@ const urlPost = 'https://jsonplaceholder.typicode.com/posts?userId='
 async function EjecucionSecuencial(){
     console.log("--- Ejecución Secuencial ---")
     for (let i = 1; i <= 3; i++) {
-        const usuario = await obtenerUsuario(urlUsuarios, i);
-        const publicaciones = await obtenerPosts(urlPost, usuario.id);
-        console.log(`${usuario.name} tiene ${publicaciones.length} publicaciones`);
+        const usuario = await obtenerUsuario(urlUsuarios, i)
+        const publicaciones = await obtenerPosts(urlPost, usuario.id)
+        console.log(usuario.name + ' tiene ' + publicaciones.length + ' publicaciones')
     }
 }
 
 async function EjecucionParalela(){
     console.log("--- Ejecucion Paralela ---")
 
-    const usuariosPromises = [1, 2, 3].map(id => obtenerUsuario(urlUsuarios, id));
-    const usuarios = await Promise.all(usuariosPromises);
+    const usuariosPromises = [1, 2, 3].map(id => obtenerUsuario(urlUsuarios, id))
+    const usuarios = await Promise.all(usuariosPromises)
 
-    const publicacionesPromises = usuarios.map(usuario => obtenerPosts(urlPost, usuario.id));
-    const publicacionesPorUsuario = await Promise.all(publicacionesPromises);
+    const publicacionesPromises = usuarios.map(usuario => obtenerPosts(urlPost, usuario.id))
+    const publicacionesPorUsuario = await Promise.all(publicacionesPromises)
 
     usuarios.forEach((usuario, index) => {
-        const publicaciones = publicacionesPorUsuario[index];
-        console.log(`${usuario.name} tiene ${publicaciones.length} publicaciones`);
+        const publicaciones = publicacionesPorUsuario[index]
+        console.log(usuario.name + ' tiene ' + publicaciones.length + ' publicaciones')
     });
 }
 
 async function main() {
-    await EjecucionSecuencial();
-    await EjecucionParalela();
+    await EjecucionSecuencial()
+    await EjecucionParalela()
 }
 
-main();
-
+main()
